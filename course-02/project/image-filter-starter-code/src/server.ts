dddf8afb14b9ddb3e5c75d8e66b29a8041e705e7
Edward1670 <edward.pis.1670@gmail.com>
2022-05-20 00:43:22 +0700

    Project 2, create new route: filteredimage to process image

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import fs from "fs";
 
 (async () => {
 
@@ -13,21 +14,37 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  // Process image route: GET /filteredimage?image_url=value
+  app.get("/filteredimage", async (req, res) => {
+      const { image_url } = req.query;
 
-  /**************************************************************************** */
+      if (!image_url) {
+          return res.send({
+              message: "Please provide an image url",
+          });
+      }
+      // If inputURL has specific character, have to encodeURI
+      const inputURL = encodeURI(image_url); 
+      try {
+          const result = await filterImageFromURL(inputURL);
+          res.sendFile(result);
+
+          // Handle delete files on /tmp/ folder , except current exported file 
+          const files = fs.readdirSync(`${__dirname}/util/tmp/`);
+          const fileNameArray = files.filter(file => {
+              return !result.includes(file);
+          }).map(data => {
+            return `${__dirname}/util/tmp/${data}`
+          })
+
+          await deleteLocalFiles(fileNameArray);
+      } catch (error) {
+          return res.status(422).send({
+            message: "Cannot process image. Something went very wrong!"
+          });
+      }
+
+  });
 
   //! END @TODO1
   
